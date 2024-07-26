7d4990d9887ccd1d34e0e51e67a88409e6937334
Danielewicz-code <134563548+Danielewicz-code@users.noreply.github.com>
2024-07-26 09:51:16 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+#small function for the dead state, basically turning every cell in the grid to 0 wich is used as initialization for the "new_state" in the "next_board_state" function
 def dead_state(w, h):
     return [[0 for _ in range(w)] for _ in range(h)]
 
